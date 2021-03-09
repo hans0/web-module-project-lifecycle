55cd7940d207c0b3c356c5ef60dbf7b74b8d8e01
hans0 <kiessler.hans@gmail.com>
2021-03-09 17:50:14 -0500

    Done with MVP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
+
+import GithubCard from './components/GithubCard';
+import GithubCardList from './components/GithubCardList';
+import githublogo from './images/githublogo.png';
+import lambdalogo from './images/lambdalogo.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const errorUser = {
+  avatar_url: githublogo,
+  bio: 'ERROR', 
+  login: 'ERROR',
+  location: 'ERROR', 
+  html_url: 'https://github.com',
+  followers: 'N/A',
+  following: 'N/A',
+  name: 'ERROR'
+}
+
+class App extends React.Component  {
+  
+  state = {
+    currentUser: 'hans0',
+    data: []
+  }
+  
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/${this.state.currentUser}`)
+      .then((res) => {
+        this.setState({...this.state, data: res.data});
+        console.log(this.state.data)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      currentUser: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios.get(`https://api.github.com/users/${this.state.currentUser}`)
+    .then((res) => {
+      this.setState({...this.state, data: res.data});
+    })
+    .catch((err) => {
+      this.setState({...this.state, data: errorUser})
+    });
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={lambdalogo} />
+          <p>❤️s</p>
+          <img src={githublogo} />
+        </header>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            onChange={this.handleChange}
+            />
+          <button>Search</button>
+        </form>
+        <GithubCard data={this.state.data}/>
+        {/* point of expansion, don't know whether to just add all the results, or get the followers below the user */}
+        {/* <GithubCardList /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/GithubCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import lambdalogo from '../images/lambdalogo.png';
+// import GithubCardList from './GithubCardList';
+
+
+class GithubCard extends React.Component {
+
+  render() {
+    return(
+      <div className="card">
+        <img src= {this.props.data.avatar_url}/>
+        <div className="card-info">
+          <h3 className="name">{this.props.data.name}</h3>
+          <p className="username">{this.props.data.login}</p>
+          <p>Location: {this.props.data.location}</p>
+          <p>Profile: <a href={this.props.data.html_url}>{this.props.data.login}</a></p>
+          <p>Followers: {this.props.data.followers}</p>
+          <p>Following: {this.props.data.following}</p>
+          <p>Biography: {this.props.data.bio}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default GithubCard;
